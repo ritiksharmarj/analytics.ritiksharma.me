fee617dc96366ec23fa4dc82f2eecc0e487d54b2
ritiksharmarj <theonlyritik@gmail.com>
2025-03-26 21:45:36 +0530

    Add security headers and logging configuration to Next.js setup; remove unused SVG files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY", // or 'SAMEORIGIN'
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
