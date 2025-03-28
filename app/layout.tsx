0720b55ad2ef7b9b3eec776943256f21df88139d
ritiksharmarj <theonlyritik@gmail.com>
2025-03-28 10:47:55 +0530

    Update font variable in globals.css and layout.tsx for improved consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-  variable: "--font-inter-var",
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
