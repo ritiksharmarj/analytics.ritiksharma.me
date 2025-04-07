ae8f233597118dfd980ca2443784697170e89372
ritiksharmarj <theonlyritik@gmail.com>
2025-04-07 21:25:41 +0530

    refactor(Home): update button to link to login page and remove subscription text

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="p-20">
-      Namaste India 🙏
-      <Button>Subscribe</Button>
+      <Button asChild>
+        <Link href="/login">Login</Link>
+      </Button>
     </div>
   );
 }
