bca26a8ecd193804e49bbb70c9ec1e074655274d
ritiksharmarj <theonlyritik@gmail.com>
2025-04-08 20:18:52 +0530

    feat: implement Google sign-in functionality and update homepage to use new component

---
+++ app/(public)/google-sign-in.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+import { GoogleIcon } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import { signIn } from "@/lib/auth/client";
+import { Loader2Icon } from "lucide-react";
+import { toast } from "sonner";
+
+export const GoogleSignIn = () => {
+  const [isPending, setIsPending] = React.useState(false);
+
+  const onGoogleSignIn = async () => {
+    await signIn.social({
+      provider: "google",
+      callbackURL: "/dashboard",
+      fetchOptions: {
+        onResponse: () => {
+          setIsPending(false);
+        },
+        onRequest: () => {
+          setIsPending(true);
+        },
+        onError: (ctx: { error: { message: string } }) => {
+          toast.error(ctx.error.message);
+        },
+      },
+    });
+  };
+
+  return (
+    <Button
+      variant="outline"
+      className="w-full"
+      onClick={onGoogleSignIn}
+      disabled={isPending}
+    >
+      {isPending ? <Loader2Icon className="animate-spin" /> : <GoogleIcon />}
+      Continue with Google
+    </Button>
+  );
+};
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,12 +1,18 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { auth } from "@/lib/auth";
+import { GoogleSignIn } from "./google-sign-in";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) redirect("/dashboard");
+
   return (
     <div className="p-20">
-      <Button asChild>
-        <Link href="/login">Login</Link>
-      </Button>
+      <GoogleSignIn />
     </div>
   );
 }
