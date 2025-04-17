36f3370c1cc39cdc6d6b8242bf63d0f0bd4e560b
ritiksharmarj <theonlyritik@gmail.com>
2025-04-17 15:15:55 +0530

    feat: add docker-compose configuration for PostgreSQL and update database connection settings

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,10 +1,22 @@
 import "dotenv/config";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { WebSocket } from "ws";
 import * as schema from "./schema";
 
-export const connection = neon(process.env.DATABASE_URL!);
+if (process.env.NODE_ENV === "production") {
+  neonConfig.webSocketConstructor = WebSocket;
+  neonConfig.poolQueryViaFetch = true;
+} else {
+  neonConfig.webSocketConstructor = WebSocket;
+  neonConfig.wsProxy = (host) => `${host}:5433/v1`;
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.pipelineTLS = false;
+  neonConfig.pipelineConnect = false;
+}
 
-export const db = drizzle(connection, { schema });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+export const db = drizzle(pool, { schema });
 
 export type db = typeof db;
