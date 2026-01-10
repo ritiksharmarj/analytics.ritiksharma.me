1f253aa1712dfdfcbc64c6482d243d4f9c2e6b4d
ritiksharmarj <theonlyritik@gmail.com>
2026-01-10 17:06:45 +0530

    feat: add jwt plugin to betterAuth configuration

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { jwt } from "better-auth/plugins";
 import { db } from "../db";
 import * as schema from "../db/schema";
 
@@ -14,4 +15,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
+  plugins: [jwt()],
 });
